7b1212bebb5d16edf81144e5b811d2a2212f2579
Anthi Koutsouveli <115115836+anthiktsvl@users.noreply.github.com>
2024-01-18 20:35:38 +0200

    Add files via upload

---
+++ WEBTECH/Health page/Health page/Js/Tips.js
@@ -0,0 +1,14 @@
+// Tipspage.js-> while scrolling down the bar dissapears
+//Eleni Petraki
+console.log("Script file loaded successfully");
+var prevScrollpos = window.scrollY;
+window.onscroll = function() {
+  var currentScrollPos = window.scrollY;
+  if (prevScrollpos > currentScrollPos) {
+    // the name change if you have id in html.(id is for only one ,class for a team )
+    document.getElementsByClassName("navbar")[0].style.top = "0";
+  } else {
+    document.getElementsByClassName("navbar")[0].style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
